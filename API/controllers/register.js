7d3d5722cd976b780408de4d87a27b1295214c7b
Mulveni <t7koii00@students.oamk.fi>
2020-11-09 16:56:18 +0200

    fixed errors when receiving malformed data

--- API/controllers/register.js
+++ API/controllers/register.js
@@ -10,35 +10,52 @@ router.post('/', [
     
     body('firstname','Firstname cannot be left blank')
     .isLength({ min: 2 }),
+    
 
     body('lastname','Lastname cannot be left blank')
     .isLength({ min: 2 }),
 
     body('email')
-    .exists()
     .isLength({ min: 6, max: 100 })
     .isEmail()
     .normalizeEmail()
-    .trim(),
-
-    body("phonenumber").isMobilePhone(),
+    .trim()
+    .custom(email =>{
+        return utils.checkEmailAvailability(email).then(user => {
+            if (user) {
+              return Promise.reject('E-mail already in use');
+            }
+        })
+    }),
+    body("phonenumber")
+    .isMobilePhone(),
 
     body("address.street",'Street cannot be left blank')
-    .isLength({ min: 1 }),
+    .isLength({ min: 2 }),
 
     body('address.postcode','Postcode cannot be left blank')
-    .isLength({ min: 1 }),
+    .isLength({ min: 2 }),
 
     body('address.city','Country cannot be left blank')
-    .isLength({ min: 1 }),
+    .isLength({ min: 2 }),
 
     body('address.country','Country cannot be left blank')
-    .isLength({ min: 1 }),
+    .isLength({ min: 2 }),
 
     body('password')
     .isLength({ min: 5 }).withMessage('Password must be at least 5 chars long')
   ],async (req, res) => {
     var result = [];
+    try {
+
+    const errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errorArray = errors.array();
+    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      res.status(422).json({message: errorArray[0].msg});
+      return;
+    }
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
     const email = req.body.email;
@@ -48,23 +65,11 @@ router.post('/', [
     const postcode = req.body.address.postcode;
     const country = req.body.address.country;
     const password = bcrypt.hashSync(req.body.password, 5);
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const validation_result = validationResult(req);
-    const errors = validationResult(req);
-    const errorArray = errors.array();
-    const value = await utils.checkEmailAvailability(email);
-    if (value) {
-        return res.status(400).json({ message:"Email already exists"});
-    }
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ message: errorArray[0].msg});
-    } else {
-        try {          
-            result = await utils.addUser(firstname,lastname,email,phonenumber,street,city,postcode,country,password);
-            res.sendStatus(201);           
-        } catch (error) {
-            res.sendStatus(500);
-        }
-    }
+    result = await utils.addUser(firstname,lastname,email,phonenumber,street,city,postcode,country,password);
+    res.sendStatus(201);
+} catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: errorArray[0].msg});
+}
 })
 module.exports = router;
