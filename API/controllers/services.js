01da3750cd95e055d994475f3e8d4dcbb0bbad25
Ville Valta <t8vavi01@students.oamk.fi>
2020-11-13 14:32:44 +0200

    Services API endpoints ready for evaluation

--- API/controllers/services.js
+++ API/controllers/services.js
@@ -1,24 +1,172 @@
+const { util } = require('chai');
 const express = require('express');
+const { getService, deleteService } = require('../utils');
+const utils = require('../utils');
 const router = express.Router();
 
-router.get('/:carId', (req, res) => {
-    res.sendStatus(200);
+router.get('/:carId', async (req, res) => {
+
+    var serviceList = [];
+
+    try {
+        serviceList = await utils.getCarServices(req.params.carId);
+
+        if (serviceList === false){
+
+            res.status(400);
+            res.json({message: 'No services for car with given ID'});
+            return;
+
+
+        } else {
+            res.json(serviceList);
+            res.status(200);
+            return;
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(404);
+        res.json({message: "NOT FOUND"});
+        return;
+    }
 });
 
-router.post('/:carId', (req, res) => {
+router.post('/:carId', async (req, res) => {
+
+    if ('mileAge' in req.body == false || req.body.mileAge == ""){
+        res.status(400);
+        res.json({message: "Mileage is missing, please enter the mileage"});
+        return;
+
+    } else {
+
+        const service = 
+
+        {
+            idCars : req.params.carId,
+            description : req.body.description,
+            mileAge : req.body.mileAge,
+            motorOilChangeDone : req.body.motorOilChangeDone,
+            motorOilChangelongLifeOilUsed : req.body.motorOilChangelongLifeOilUsed,
+            airConditioningServiceDone : req.body.airConditioningServiceDone,
+            airConditioningServiceDryer : req.body.airConditioningServiceDryer,
+            sparkPlugReplacement : req.body.sparkPlugReplacement,
+            airFilterReplacement : req.body.airFilterReplacement,
+            cleanAirReplacement : req.body.cleanAirReplacement,
+            fuelFilterReplacement : req.body.fuelFilterReplacement,
+            brakeFluidReplacement : req.body.brakeFluidReplacement,
+            gearBoxOilReplacement : req.body.gearBoxOilReplacement,
+            powerSteeringOilReplacement : req.body.powerSteeringOilReplacement,
+            timingBeltReplacement : req.body.timingBeltReplacement,
+            waterPumpReplacement : req.body.waterPumpReplacement,
+            dieselParticulateFilterReplacement : req.body.dieselParticulateFilterReplacement,
+            additionalInformation : req.body.additionalInformation
+        };
+
+        try {
+            await utils.addNewService(service.idCars, service.description, service.mileAge, service.motorOilChangeDone, service.motorOilChangelongLifeOilUsed, service.airConditioningServiceDone, 
+                service.airConditioningServiceDryer, service.sparkPlugReplacement, service.airFilterReplacement, service.cleanAirReplacement, service.fuelFilterReplacement, service.brakeFluidReplacement, 
+                service.gearBoxOilReplacement, service.powerSteeringOilReplacement, service.timingBeltReplacement, service.waterPumpReplacement, service.dieselParticulateFilterReplacement, 
+                service.additionalInformation);
+
+            res.sendStatus(201);
+
+        } catch (error) {
+            console.log(error);
+            res.status(400);
+            res.json({message: "Failed to create service, missing obligatory information or car you tried to create service to doesn't exist!"});
+        }
+    }
+
+});
+
+router.get('/:carId/:serviceId', async (req, res) => {
+
+    serviceList = [];
+
+    try {
+        serviceList = await getService(req.params.carId, req.params.serviceId);
+
+        if (serviceList === false) {
+
+            res.status(400);
+            res.json({message: "No service found with the given id"});
+        } else {
+            res.json(serviceList);
+            res.status(200);
+        }
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(404);
+    }
+});
+
+router.put('/:carId/:serviceId', async (req, res) => {
+
+    const editService = {
+        motorOilChangeDone: null,
+        motorOilChangelongLifeOilUsed: null,
+        airConditioningServiceDone: null,
+        airConditioningServiceDryer: null, 
+        sparkPlugReplacement: null, 
+        airFilterReplacement: null,
+        cleanAirReplacement: null, 
+        fuelFilterReplacement: null, 
+        brakeFluidReplacement: null,
+        gearBoxOilReplacement: null, 
+        powerSteeringOilReplacement: null, 
+        timingBeltReplacement: null, 
+        waterPumpReplacement: null, 
+        dieselParticulateFilterReplacement: null,
+        additionalInformation: null
+    };
+
+    for (const key in req.body) {
+        editService[key] = req.body[key];
+    }
+
+    try {
+        serviceEdit = await utils.editService(req.params.carId, req.params.serviceId, editService);
+
+        if (serviceEdit === null){
+            res.status(404);
+            res.json({message: "Cars has no service matching the id"});
+        } else {
+            res.status(201);
+            res.json(serviceEdit);
+        }
+
+    } catch (error){
+        console.log(error);
+        res.status(400);
+        res.json({message: "Editing infromation failed!"});
+
+    }
     res.sendStatus(201);
 });
 
-router.get('/:carId/:serviceId', (req, res) => {
-    res.sendStatus(200);
-});
+router.delete('/:carId/:serviceId', async (req, res) => {
 
-router.put('/:carId/:serviceId', (req, res) => {
-    res.sendStatus(201);
-});
+    var deleteService = [];
 
-router.delete('/:carId/:serviceId', (req, res) => {
-    res.sendStatus(201);
+    try {
+        deleteService = await utils.deleteService(req.params.carId, req.params.serviceId);
+
+        if (deleteService.affectedRows === 0){
+            res.status(400);
+            res.json({message: "Failed to delete!"});
+        } else {
+            res.status(201);
+            res.json({message: "Succesfully deleted the selected service!"});
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(400);
+    }
+
 });
 
 module.exports = router;
--- API/utils.js
+++ API/utils.js
@@ -1,5 +1,6 @@
 const db = require('./database');
 const bcrypt = require('bcryptjs');
+const { serializeUser } = require('passport');
 
 const utils = {
     checkUserLogin: (username, password) => {
@@ -16,7 +17,136 @@ const utils = {
                 reject(error);
             });
         });
+    },
+
+    getCarServices: (idCars) => {
+        return new Promise(function (resolve, reject) {
+            db.query('SELECT * FROM services WHERE idCars = ?', idCars).then(results => {
+                    if (results.length !== 0) {
+                        console.log(results);
+                        resolve(results);
+                        return;
+
+                    } else {
+                        resolve(false);
+                        return;
+                    }
+
+            }).catch(error => {
+                console.log(error);
+                reject(error);
+            });
+        });
+    },
+
+    addNewService: (idCars, description, mileAge, motorOilChangeDone, motorOilChangelongLifeOilUsed, airConditioningServiceDone, airConditioningServiceDryer, sparkPlugReplacement, airFilterReplacement, cleanAirReplacement, fuelFilterReplacement, brakeFluidReplacement, gearBoxOilReplacement, powerSteeringOilReplacement, timingBeltReplacement, waterPumpReplacement, dieselParticulateFilterReplacement, additionalInformation) => {
+        return new Promise(function (resolve, reject) {
+            db.query("INSERT INTO services(idCars, description, mileAge, motorOilChangeDone, motorOilChangelongLifeOilUsed, airConditioningServiceDone, airConditioningServiceDryer, sparkPlugReplacement, airFilterReplacement, cleanAirReplacement, fuelFilterReplacement, brakeFluidReplacement, gearBoxOilReplacement, powerSteeringOilReplacement, timingBeltReplacement, waterPumpReplacement, dieselParticulateFilterReplacement, additionalInformation) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+            [idCars, description, mileAge, motorOilChangeDone, motorOilChangelongLifeOilUsed, airConditioningServiceDone, airConditioningServiceDryer, sparkPlugReplacement, airFilterReplacement, cleanAirReplacement, fuelFilterReplacement, brakeFluidReplacement, gearBoxOilReplacement, powerSteeringOilReplacement, timingBeltReplacement, waterPumpReplacement, dieselParticulateFilterReplacement, additionalInformation])
+            .then(results => {
+                console.log(results);
+                resolve(results);
+
+            }).catch(error => {
+                console.log(error);
+                reject(error);
+            });
+
+        });
+    },
+
+    getService: (idCars, idServices) => {
+        return new Promise(function(resolve, reject) {
+            db.query('SELECT * FROM services WHERE idCars = ? AND idServices = ?', [idCars, idServices]).then(results => {
+                if (results.length !== 0) {
+                    console.log(results);
+                    resolve(results);
+                } else {
+                    resolve(false);
+                }
+            }).catch(error => {
+                console.log(error);
+                reject(error);
+            });
+        });
+    },
+
+    editService: (idCars, idServices, service) => {
+        return new Promise(function(resolve, reject) {
+            db.query('SELECT * FROM services WHERE idCars = ? AND idServices = ?', [idCars, idServices]).then(results => {
+                if (results.length !== 0){
+                    if (service.motorOilChangeDone === null){
+                        service.motorOilChangeDone = results[0].motorOilChangeDone;
+                    }
+                    if (service.motorOilChangelongLifeOilUsed === null){
+                        service.motorOilChangelongLifeOilUsed = results[0].motorOilChangelongLifeOilUsed;
+                    }
+                    if (results.airConditioningServiceDone === null){
+                        service.airConditioningServiceDone = results[0].airConditioningServiceDone;
+                    }
+                    if (results.airConditioningServiceDryer === null){
+                        service.airConditioningServiceDryer = results[0].airConditioningServiceDryer;
+                    }
+                    if (results.sparkPlugReplacement === null){
+                        service.sparkPlugReplacement = results[0].sparkPlugReplacement;
+                    }
+                    if (results.airFilterReplacement === null){
+                        service.airFilterReplacement = results[0].airFilterReplacement;
+                    }
+                    if (results.cleanAirReplacement === null){
+                        service.cleanAirReplacement = results[0].cleanAirReplacement;
+                    }
+                    if (results.fuelFilterReplacement === null){
+                        service.fuelFilterReplacement = results[0].fuelFilterReplacement;
+                    }
+                    if (results.brakeFluidReplacement === null){
+                        service.brakeFluidReplacement = results[0].brakeFluidReplacement;
+                    }
+                    if (results.gearBoxOilReplacement === null){
+                        service.gearBoxOilReplacement = results[0].gearBoxOilReplacement;
+                    }
+                    if (results.powerSteeringOilReplacement === null){
+                        service.powerSteeringOilReplacement = results[0].powerSteeringOilReplacement;
+                    }
+                    if (results.timingBeltReplacement === null){
+                        service.timingBeltReplacement = results[0].timingBeltReplacement;
+                    }
+                    if (results.waterPumpReplacement === null){
+                        service.waterPumpReplacement = results[0].waterPumpReplacement;
+                    }
+                    if (results.dieselParticulateFilterReplacement === null){
+                        service.dieselParticulateFilterReplacement = results[0].dieselParticulateFilterReplacement;
+                    }
+                    if (results.additionalInformation === null){
+                        service.additionalInformation = results[0].additionalInformation;
+                    }
+                    
+                    resolve(db.query('UPDATE services SET motorOilChangeDone = ?, motorOilChangelongLifeOilUsed = ?, airConditioningServiceDone = ?, airConditioningServiceDryer = ?, sparkPlugReplacement = ?, airFilterReplacement = ?, cleanAirReplacement = ?, fuelFilterReplacement = ?, brakeFluidReplacement = ?, gearBoxOilReplacement = ?, powerSteeringOilReplacement = ?, timingBeltReplacement = ?, waterPumpReplacement = ?, dieselParticulateFilterReplacement = ?, additionalInformation = ? WHERE idCars = ? AND idServices = ?',
+                    [service.motorOilChangeDone, service.motorOilChangelongLifeOilUsed, service.airConditioningServiceDone, service.airConditioningServiceDryer, service.sparkPlugReplacement, service.airFilterReplacement, service.cleanAirReplacement, service.fuelFilterReplacement, service.brakeFluidReplacement , service.gearBoxOilReplacement , service.powerSteeringOilReplacement, service.timingBeltReplacement , service.waterPumpReplacement, service.dieselParticulateFilterReplacement, service.additionalInformation, idCars, idServices]));
+                } else {
+                    resolve(false);
+                }
+            }).catch(error => {
+                console.log(error);
+                reject(error);
+            });
+        });
+    },
+
+    deleteService: (idCars, idServices) => {
+        return new Promise(function(resolve, reject) {
+            db.query('DELETE FROM services WHERE idCars = ? AND idServices = ?', [idCars, idServices]).then(results => {
+                console.log("Results are: ");
+                console.log(results);
+                resolve(results);
+
+            }).catch(error => {
+                console.log(error);
+                reject(error);
+            });
+        });
     }
+
 }
 
 module.exports = utils;
