53919ed8773b3eef224815bb7cfc77b49708d677
Mulveni <t7koii00@students.oamk.fi>
2020-11-18 20:42:50 +0200

    fixed some ui problems

--- WEB/src/components/Register.js
+++ WEB/src/components/Register.js
@@ -2,31 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { hideNavButtons } from '../actions';
 import { useTranslation } from 'react-i18next';
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
 import baseApiUrl from '../api_url.json';
 import axios from 'axios';
 
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(3),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -36,30 +28,22 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(3, 0, 2),
   },
 }));
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-const emailIsUnique = async (email: string) => {
-  await sleep(1000);
-  return email !== "someone@somewhere.com";
-};
 
 const Register = () => {
 
  
   const apiUrl = baseApiUrl.url;
-    const [countries, setCountry]= useState("");
 
     const classes = useStyles();
     const { register, errors, handleSubmit } = useForm({
       mode: "onBlur",
     });
     const onSubmit = data => {
-      alert(JSON.stringify(data));
 
       axios.post(`${apiUrl}/register`, data, {
       })
       .then((response) => {
-        console.log(response);
+
       }, (error) => {
         console.log(error);
       });
@@ -74,26 +58,23 @@ const Register = () => {
 
     return (
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
         <div className={classes.paper}>
-          <Typography component="h1" variant="h5">
-            Register
+          <Typography component="h6" variant="h6">
+          {t('register')}
           </Typography>
           <form className={classes.form} onSubmit={handleSubmit(onSubmit, onError)}>
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-              required: "required", 
+                required: true,
               pattern: {
                 value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
                 message: 'Invalid email address',
               },
               minLength: 2,
-              validate: emailIsUnique
               })}
               fullWidth
-
               id="email"
               label={t('email')}
               name="email"
@@ -104,7 +85,7 @@ const Register = () => {
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true,
                 minLength: 5,
               })}
               fullWidth
@@ -118,7 +99,7 @@ const Register = () => {
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true,
                 minLength: 5,
               })}
               fullWidth
@@ -127,12 +108,12 @@ const Register = () => {
               type="phonenumber"
               id="phonenumber"
             />
-            {errors.phonenumber && "phonenumber is required and must be 5 chars long"}
+            {errors.phonenumber && "Phonenumber is required and must be 5 chars long"}
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true, 
                 minLength: 2,
               })}
               fullWidth
@@ -141,12 +122,12 @@ const Register = () => {
               type="etunimi"
               id="firstname"
             />
-            {errors.etunimi && "Firstname is required"}
+            {errors.firstname && "Firstname is required"}
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true,
                 minLength: 2,
               })}
               fullWidth
@@ -155,12 +136,12 @@ const Register = () => {
               type="sukunimi"
               id="lastname"
             />
-            {errors.sukunimi && "Lastname is required"}
+            {errors.lastname && "Lastname is required"}
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true, 
                 minLength: 2,
               })}
               fullWidth
@@ -169,12 +150,12 @@ const Register = () => {
               type="osoite"
               id="street"
             />
-            {errors.osoite && "Address is required"}
+            {errors.street && "Street is required"}
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true,
                 minLength: 2,
               })}
               fullWidth
@@ -183,12 +164,12 @@ const Register = () => {
               type="postinumero"
               id="city"
             />
-            {errors.postinumero && "Postcode is required"}
+            {errors.city && "City is required"}
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true,
                 minLength: 2,
               })}
               fullWidth
@@ -197,12 +178,12 @@ const Register = () => {
               type="postitoimipaikka"
               id="postcode"
             />
-            {errors.postitoimipaikka && "Post office is required"}
+            {errors.postcode && "Postcode is required"}
             <TextField
               variant="outlined"
               margin="normal"
               inputRef={register({
-                required: "required", 
+                required: true,
                 minLength: 2,
               })}
               fullWidth
