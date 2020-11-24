56a0a178c3cdacb2966432d3239ef7a8e153181c
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-11-24 16:46:59 +0200

    Removed unused things

--- WEB/src/components/Error.js
+++ WEB/src/components/Error.js
@@ -1,16 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { hideNavButtons } from '../actions';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Error = () => {
 
     const { t } = useTranslation();
-
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(hideNavButtons());
-    }, [dispatch]);
 
     return (
         <div>
