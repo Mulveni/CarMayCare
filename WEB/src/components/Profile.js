813ee420d5a2d6daa9f286f96bd452b9a3a0337e
Mulveni <t7koii00@students.oamk.fi>
2020-11-24 10:33:26 +0200

    added imports

--- WEB/src/components/Profile.js
+++ WEB/src/components/Profile.js
@@ -2,6 +2,20 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { showNavButtons } from '../actions';
+import { useForm, Controller } from "react-hook-form";
+import baseApiUrl from '../api_url.json';
+import axios from 'axios';
+import {
+    Container,
+    Button,
+    TextField, 
+    Typography, 
+    Select, 
+    FormControl, 
+    MenuItem, 
+    InputLabel,
+    FormHelperText
+    } from "@material-ui/core";
 
 const Profile = () => {
 
