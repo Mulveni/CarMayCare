42a3aaf2066e175c7297084932b521eeae7c640f
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-11-26 11:17:50 +0200

    Error page styling

--- WEB/src/components/Error.js
+++ WEB/src/components/Error.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Grid, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles({
+    link: {
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 25
+    }
+});
 
 const Error = () => {
-
     const { t } = useTranslation();
+    const classes = useStyles();
 
     return (
         <div>
-            Server error
+            <Grid container justify="center" direction="column" alignItems="center" style={{ marginTop: 50 }}>
+                <h1>{t('internal_server_error')}</h1>
+                <Link className={classes.link} to="/login" >{t('try_to_login')}</Link>
+            </Grid>
         </div >
     )
 }
