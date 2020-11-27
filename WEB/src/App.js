c178a83b2886b63b4416da729ce44cad19d7a0bd
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-11-27 14:35:32 +0200

    Change default font

--- WEB/src/App.js
+++ WEB/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch, withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import Navigation from './components/Navigation';
 import Register from './components/Register';
@@ -11,42 +11,43 @@ import MyCars from './components/MyCars';
 import CarView from './components/CarView';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 const App = () => {
+    const theme = createMuiTheme({
+        typography: {
+            fontFamily: 'Quicksand'
+        },
+        overrides: {
+            MuiLink: {
+                root: {
+                    fontFamily: 'Quicksand',
+                    color: "#304269"
+                },
+            }
+        }
+    });
+
     const isLoggedin = useSelector(state => state.loginReducer);
 
     return (
         <div>
-            <Router>
-                <Navigation />
-                <Switch>
-                    <Route exact path="/">
-                        {!isLoggedin ? <Redirect to="/login" /> : <Profile />}
-                    </Route>
-
-                    <Route path="/register" component={Register} />
-                    <Route path="/login" exact component={Login} />
-                    <Route path="/forgotpassword" exact component={ForgotPassword} />
-
-                    <Route exact path="/carview">
-                        {!isLoggedin ? <Redirect to="/login" /> : <CarView />}
-                    </Route>
-
-                    <Route exact path="/mycars">
-                        {!isLoggedin ? <Redirect to="/login" /> : <MyCars />}
-                    </Route>
-
-                    <Route exact path="/addcar">
-                        {!isLoggedin ? <Redirect to="/login" /> : <AddCar />}
-                    </Route>
-
-                    <Route path="/resetpassword/:id" exact component={ResetPassword} />
-                    <Route component={NotFound} />
-                </Switch>
-
-
-
-            </Router>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Navigation />
+                    <Switch>
+                        <Route exact path="/">{!isLoggedin ? <Redirect to="/login" /> : <Profile />}</Route>
+                        <Route path="/register" component={Register} />
+                        <Route path="/login" exact component={Login} />
+                        <Route path="/forgotpassword" exact component={ForgotPassword} />
+                        <Route exact path="/carview">{!isLoggedin ? <Redirect to="/login" /> : <CarView />}</Route>
+                        <Route exact path="/mycars">{!isLoggedin ? <Redirect to="/login" /> : <MyCars />}</Route>
+                        <Route exact path="/addcar">{!isLoggedin ? <Redirect to="/login" /> : <AddCar />}</Route>
+                        <Route path="/resetpassword/:id" exact component={ResetPassword} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Router>
+            </ThemeProvider>
         </div >
     )
 }
