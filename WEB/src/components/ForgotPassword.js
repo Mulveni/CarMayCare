605cbbb42a677be8844b9fd3fd50b0dd9fb078f8
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-12-08 16:10:16 +0200

    Fix warning

--- WEB/src/components/ForgotPassword.js
+++ WEB/src/components/ForgotPassword.js
@@ -37,7 +37,7 @@ const ForgotPassword = () => {
         return (
             <div>
                 <Card className={classes.background} style={{ marginTop: 50 }}>
-                    <Grid container xs={12} direction="column" justify="center" alignItems="center" style={{ paddingTop: 25, paddingBottom: 25 }}>
+                    <Grid container item xs={12} direction="column" justify="center" alignItems="center" style={{ paddingTop: 25, paddingBottom: 25 }}>
                         <Typography variant="h5">
                             {t('forgot_password_sent_message')}
                         </Typography>
