a6303d4b783454c13f388fa4f93105e3387dcdc8
Mulveni <t7koii00@students.oamk.fi>
2020-12-11 10:25:30 +0200

    fixed typos in addNote,Navigation and translation_en

--- WEB/src/components/AddNote.js
+++ WEB/src/components/AddNote.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import baseApiUrl from "../api_url.json";
