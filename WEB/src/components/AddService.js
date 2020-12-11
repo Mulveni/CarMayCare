8e196a96d9761239d09e084605b5fb4e8a651a9d
Ville Vaara <ville.vaara7@gmail.com>
2020-12-11 11:21:18 +0200

    Removed unused imports

--- WEB/src/components/AddService.js
+++ WEB/src/components/AddService.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 import baseApiUrl from '../api_url.json';
